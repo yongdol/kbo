fd079f3494afb359fafc09871d74cb0754a69d94
yongdol <kungbuk77@gmail.com>
2017-04-11 04:05:40 +0900

    make file crawling data

--- get_articles.py
+++ get_articles.py
@@ -5,31 +5,30 @@ from selenium import webdriver
 driver = webdriver.Chrome('/Users/yongjoolim/Downloads/chromedriver')
 today = datetime.datetime.now().strftime("%Y%m%d")
 url = 'http://sports.news.naver.com/kbo/news/index.nhn?view=text&type=team&team=OB&date=' + today
+url = 'http://sports.news.naver.com/kbo/news/index.nhn?view=text&type=team&team=OB&date=20170410'
 
 driver.get(url)
 driver.implicitly_wait(3)
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-# news_list_div = soup.select('div.news_list > ul > li > div.text')
-# for news_list in news_list_div:
-#     a_tag = news_list.find('a')
-#     news_link = a_tag.get('href')
-#     print(news_link)
-#     news_title = news_list.select('a.title > span')
-#     print(news_title)
-#     news_contents = news_list.find('p')
-#     print(news_contents)
-# driver.quit()
-
-
-
 news_list_div = soup.select('div.news_list > ul > li > div.text')
+TEMPLATES_DIR = './django_app/templates/articles/OB/'
+f = open(TEMPLATES_DIR + today + ".txt", 'w')
 for news_list in news_list_div:
     a_tag = news_list.find('a')
     news_link = a_tag.get('href')
-    print(news_link)
-    news_title = news_list.find('a.title > span')
-    print(news_title)
-    news_contents = news_list.find('p')
-    print(news_contents)
+    news_title = str(a_tag.find('span'))[6:][:-7]
+    # news_title = str(a_tag.find('span')).replace('<span>', '').replace('</span>', '')
+    news_contents = str(news_list.find('p'))[16:][:-4]
+    # news_contents = str(news_list.find('p')).replace('<p class="desc">', '').replace('</p>', '')
+    row = news_title + '\t' + news_contents + '\t' + news_link + '\n'
+    # print(row)
+    f.write(row)
+    # print(news_link)
+    # print(type(news_title))
+    # print(news_title)
+    # print(type(news_contents))
+    # print(news_contents)
+f.close()
+
 driver.quit()
