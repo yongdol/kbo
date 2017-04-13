3a3ffb49fc1ac845f706695ae478fe17b9ed728c
yongdol <kungbuk77@gmail.com>
2017-04-14 04:10:39 +0900

    get totalPages

--- crawling_use_txt.py
+++ crawling_use_txt.py
@@ -1,5 +1,7 @@
+import json
 import os
 
+import re
 from bs4 import BeautifulSoup
 import requests
 
@@ -7,19 +9,37 @@ team_dict = {'HH': '7'}
 today = '20170410'
 
 
-def crawling_data_make_file():
+def make_article_file():
     for team, team_id in team_dict.items():
         template_dir = './django_app/templates/articles/' + team + '/'
         file_name = template_dir + today + '_' + team + "_test.txt"
-        url = 'http://sports.news.naver.com/kbo/news/index.nhn?view=text&type=team&team=' + team + '&date=' + today
-        html_doc = requests.get(url)
-        # print(html_doc)
-        soup = str(BeautifulSoup(html_doc.text, 'lxml'))
-        f = open(file_name, 'w')
-        f.write(soup)
-        f.close()
+        base_url = 'http://sports.news.naver.com/kbo/news/index.nhn?view=text&type=team&team=' + team + '&date=' + today
+        data = requests.get(base_url)
+        html_doc = BeautifulSoup(data.text, 'lxml')
+
+        # Parsing (total pages)
+        match = re.search("totalPages\":.", html_doc.text)
+        total_page = int(match.group(0).split(":")[1])
+
+        # soup = str(BeautifulSoup(html_doc.text, 'lxml')).split('\n')
+        # f = open(file_name, 'w+')
+        # f.write(soup)
+        # for line in soup:
+        #     if line.__contains__("newsListModel"):
+        #         article_str = line.strip().replace('newsListModel: ', '')
+        #         print(article_str)
+        #         print(type(article_str))
+                # article_dic = dict(article_str)
+                # print(article_dic)
+                # print(type(article_dic))
+                # for i in range(len(article_dic["list"])):
+                #     print(i)
+                # f.write(article_dic)
+                # else:
+                #     pass
+        # f.close()
+
+make_article_file()
 
 
-
-
-crawling_data_make_file()
+# def save_article():
