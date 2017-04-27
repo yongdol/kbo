8b391904dda642343575c9ec377254983ed9fdb9
yongdol <kungbuk77@gmail.com>
2017-04-28 02:09:34 +0900

    add hitterrank

--- django_app/game/models.py
+++ django_app/game/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from article.models import Team
 
 
-class TeamRank(models.Model):
+class Teamrank(models.Model):
     team = models.ForeignKey(Team)
     rank = models.IntegerField(default=0)
     win = models.IntegerField(default=0)
@@ -15,7 +15,71 @@ class TeamRank(models.Model):
     continuous = models.CharField(max_length=5, blank=True)
     home_score = models.CharField(max_length=10, blank=True)
     away_score = models.CharField(max_length=10, blank=True)
-    standard_date = models.CharField(max_length=32)
+    standard_date = models.IntegerField(blank=True)
 
     def __str__(self):
         return self.team
+
+
+class Hiterrank(models.Model):
+    player = models.CharField(max_length=10)
+    team = models.ForeignKey(Team)
+    avg = models.FloatField(default=0.0)
+    g = models.IntegerField(default=0)
+    pa = models.IntegerField(default=0)
+    ab = models.IntegerField(default=0)
+    r = models.IntegerField(default=0)
+    h = models.IntegerField(default=0)
+    two_base = models.IntegerField(default=0)
+    three_base = models.IntegerField(default=0)
+    hr = models.IntegerField(default=0)
+    total_base = models.IntegerField(default=0)
+    rbi = models.IntegerField(default=0)
+    sac = models.IntegerField(default=0)
+    sf = models.IntegerField(default=0)
+    bb = models.IntegerField(default=0)
+    ibb = models.IntegerField(default=0)
+    hbp = models.IntegerField(default=0)
+    so = models.IntegerField(default=0)
+    gdp = models.IntegerField(default=0)
+    slg = models.FloatField(default=0.000)
+    obp = models.FloatField(default=0.000)
+    ops = models.FloatField(default=0.000)
+    mh = models.IntegerField(default=0)
+    risp = models.FloatField(default=0.000)
+    ph_ba = models.FloatField(default=0.000)
+    standard_date = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.player
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
